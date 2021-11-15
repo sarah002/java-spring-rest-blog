20bb54a3f16b26f2ed56d858ce3ea5f22edf16c8
sarah002 <33877944+sarah002@users.noreply.github.com>
2021-11-15 15:47:13 +0000

    add Autowired annotation

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -3,6 +3,7 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -22,6 +23,7 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
 
+    @Autowired
     public DatabaseLoader(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
